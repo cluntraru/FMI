4afa9d771fba7493bf522e0272a68840e47271b1
Chris Luntraru <chris.luntraru@gmail.com>
2019-05-13 14:14:35 +0300

    IA Proj

--- Y2-S2/ia/half-2/project/svm.py
+++ Y2-S2/ia/half-2/project/svm.py
@@ -4,10 +4,12 @@ import sklearn.metrics as sm
 from sklearn import svm
 from sklearn import preprocessing
 from os import listdir
+from random import randint
 
 
 def svm_classifier(train_data, train_labels, test_data, c):
     svm_model = svm.SVC(C = c, kernel = "linear")
+#    svm_model = svm.SVC(C = c, kernel = "linear", gamma = "scale")
     svm_model.fit(train_data, train_labels)
     pred_train_labels = svm_model.predict(train_data)
     pred_test_labels = svm_model.predict(test_data)
@@ -50,9 +52,69 @@ def reading_avg(reading):
     return avg
 
 
+def reading_abs_movement(reading):
+    abs_move = [0, 0, 0]
+    for i in range(1, len(reading)):
+        for j in range(3):
+            abs_move[j] += abs(reading[i][j] - reading[i - 1][j])
+
+    return abs_move
+
+
+def get_segment_values(segment):
+    x_col = segment[:,0]
+    y_col = segment[:,1]
+    z_col = segment[:,2]
+    x_col = np.sort(x_col)
+    y_col = np.sort(y_col)
+    z_col = np.sort(z_col)
+    x_vals = [x_col[0], x_col[-1], x_col[len(x_col) // 2]]
+    y_vals = [y_col[0], y_col[-1], y_col[len(y_col) // 2]]
+    z_vals = [z_col[0], z_col[-1], z_col[len(z_col) // 2]]
+    return x_vals + y_vals + z_vals
+
+
 def process_reading(reading):
     ''' Turn accelerometer reading into a 3-tuple '''
-    return reading_avg(reading) 
+    processed_reading = np.array([])
+    segment_num = 13
+#    segment_len = len(reading) // segment_num
+    segment_len = 150 // segment_num
+    for segment_idx in range(segment_num - 1):
+        segment = reading[segment_idx * segment_len : (segment_idx + 1) * segment_len][:]
+        segment_vals = get_segment_values(segment)
+        processed_reading = np.append(processed_reading, segment_vals, axis = 0)
+   
+    # Assuming that no readings are shorter than 135
+    segment = reading[(segment_num - 1) * segment_len : len(reading)][:]
+    segment_vals = get_segment_values(segment)
+    processed_reading = np.append(processed_reading, segment_vals, axis = 0)
+    
+    processed_reading = processed_reading.flatten()
+    return processed_reading
+    
+
+#    app = {}
+#    nums = []
+    # 45 equally distanced numbers
+#    for i in range(45):
+#        nums.append(i * 3)
+
+#    processed_reading = np.array([])
+#    for i in range(100):
+#        rd = randint(0, len(reading) - 1)
+#        while rd in app and app[rd] == True:
+#            rd = randint(0, len(reading) - 1)
+
+#        app[rd] = True
+#        nums.append(rd)
+
+#    for row in nums:
+#        processed_reading = np.append(processed_reading, reading[row][:], axis = 0)
+
+#    processed_reading = processed_reading.flatten()
+#    reading = reading[:100,:].flatten()
+#    return processed_reading
 
 
 def data_from_dir(data_path, dir_name):
@@ -68,7 +130,8 @@ def data_from_dir(data_path, dir_name):
         data = np.append(data, reading, axis = 0)
         ids = np.append(ids, curr_id)
 
-    data = np.reshape(data, (len(files), 3))
+    data = np.reshape(data, (len(files), 117))
+#    data = np.reshape(data, (len(files), 135))
     return data, ids
 
 
@@ -81,17 +144,20 @@ train_labels = train_labels[:,1]
 
 train_data, train_ids = data_from_dir(data_path, "train/")
 test_data, test_ids = data_from_dir(data_path, "test/")
-
 scaled_train_data, scaled_test_data = normalize_data(train_data, test_data, "standard")
 pred_train_labels, pred_test_labels = svm_classifier(scaled_train_data, train_labels, scaled_test_data, 1) 
 
 out_file_name = data_path + "output.csv"
 out_file = open(out_file_name, "w") 
 
+test_labels = np.genfromtxt(data_path + "test_labels0.csv", delimiter = ",", skip_header = 1)
+test_labels = test_labels[:,1]
+
 print(pred_train_labels, train_labels)
 print("Training accuracy: ", sm.accuracy_score(pred_train_labels, train_labels))
+#print("Test accuracy: ", sm.accuracy_score(pred_test_labels, test_labels))
 
 out_file.write("id,class\n")
 for i in range(len(pred_test_labels)):
-    out_file.write(str(test_ids[i]) + "," + str(pred_test_labels[i]) + "\n")
+    out_file.write(str(test_ids[i]) + "," + str(int(pred_test_labels[i])) + "\n")
 
