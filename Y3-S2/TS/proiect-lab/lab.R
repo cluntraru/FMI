d004a774fad7ff9ffc1f884c5b41a76346ece389
Chris Luntraru <chris.luntraru@gmail.com>
2020-05-15 07:29:13 +0300

    TS

--- Y3-S2/TS/proiect-lab/lab.R
+++ Y3-S2/TS/proiect-lab/lab.R
@@ -1,3 +1,4 @@
+# Functia de intensitate din enunt
 intensity <- function(t) {
   if (t < 0 || t > 24) {
     return(-1)
@@ -10,15 +11,21 @@ intensity <- function(t) {
   }
 }
 
+# Functie care intoarce timpul la care se petrece urmatorul eveniment Ts
 genPoisson <- function(s, lambda, intensity) {
+  # Initial, ne aflam la momentul de timp s
   t <- s
-  
+  # Generam U1 si U2
   U <- runif(2)
+  # Actualizam momentul de timp curent
   t <- t - 1 / lambda * log(U[1])
+  # Regeneram U1, U2 cat timp nu se indeplineste U2 <= lambda(t)/t
   while (U[2] > intensity(t) / lambda) {
     U <- runif(2)
+    # Actualizam timpul, analog cu mai sus
     t <- t - 1 / lambda * log(U[1])
   }
   
+  # Daca este indeplinita conditia, atunci Ts = t, deci returnam t
   return(t)
 }
